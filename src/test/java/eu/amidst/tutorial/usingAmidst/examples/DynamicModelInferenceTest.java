3671f5f4c8e6a4c770905fa029b00d7a3bcf490a
Rafael Cabañas de Paz <rafacabanas@gmail.com>
2016-07-06 16:39:44 +0200

    issue #1: added examples tests

---
+++ src/test/java/eu/amidst/tutorial/usingAmidst/examples/DynamicModelInferenceTest.java
@@ -0,0 +1,23 @@
+package eu.amidst.tutorial.usingAmidst.examples;
+
+import junit.framework.TestCase;
+import org.junit.Test;
+
+
+/**
+ * Created by rcabanas on 20/05/16.
+ */
+
+
+public class DynamicModelInferenceTest extends TestCase {
+
+    @Test
+    public void test() throws Exception {
+
+        System.out.println("Working Directory = " +
+                System.getProperty("user.dir"));
+
+        DynamicModelInference.main(null);
+    }
+
+}
